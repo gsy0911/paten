13b81dce679f57063f65e13b24456ca5270e6cac
yoshiki <yoshiki0911@gmail.com>
2020-09-03 15:41:36 +0900

    #4 constantsのexmplae_projectの修正

--- paten/constants.py
+++ paten/constants.py
@@ -1,4 +1,5 @@
 TEMPLATE_APP = """
+import azure.functions as func
 from paten import Paten
 
 app = Paten('%s')
@@ -6,14 +7,24 @@ app = Paten('%s')
 
 @app.http_trigger('req', methods=['GET'], route='/')
 @app.out_http()
-def example_http_function(req):
-    pass
+def example_http_function(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    
+    # response
+    return func.HttpResponse(name)
 
 
 @app.http_trigger('req', methods=['GET'], route='/')
-@app.out_queue('arg_name', queue_name='example_queue')
-def example_http_queue_function(req, arg_name):
-    pass
+@app.out_queue('your_queue', queue_name='example_queue')
+@app.out_http()
+def example_http_queue_function(req: func.HttpRequest, your_queue: func.Out[str]) -> func.HttpResponse:
+    name = req.params.get('name')
+    # set name in your queue
+    your_queue.set(name)
+    
+    # response
+    return func.HttpResponse(name)
+    
 """
 
 
@@ -23,14 +34,12 @@ GITIGNORE = """
 
 
 WELCOME_PROMPT = r"""
-
  _____             _______  ______  _   _
 |  __ \     /\    |__   __||  ____|| \ | |
 | |__) |   /  \      | |   | |__   |  \| |
 |  ___/   / /\ \     | |   |  __|  | . ` |
 | |      / ____ \    | |   | |____ | |\  |
 |_|     /_/    \_\   |_|   |______||_| \_|
-
 
 Welcome PATEN.
 Your function app `%s` is generated
