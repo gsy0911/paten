b917a199bdc0c98d3310534f79cf0c8db7020630
yoshiki <yoshiki0911@gmail.com>
2020-09-03 14:19:40 +0900

    #4 各commandにctxにてfactoryを渡すように修正

--- paten/cli/__init__.py
+++ paten/cli/__init__.py
@@ -3,23 +3,34 @@ import os
 import click
 
 from paten.constants import TEMPLATE_APP, GITIGNORE
+from .factory import CliFactory
 
 
 @click.group(invoke_without_command=True)
+@click.option('--function-app-dir')
 @click.pass_context
-def cmd(ctx):
+def cmd(ctx, function_app_dir: str):
+    if function_app_dir is None:
+        function_app_dir = os.getcwd()
     if ctx.invoked_subcommand is None:
         click.echo('This is parent!')
+    ctx.obj['function_app_dir'] = function_app_dir
+    ctx.obj['factory'] = CliFactory(function_app_dir=function_app_dir)
 
 
 @cmd.command("deploy")
-def deploy():
+@click.pass_context
+def deploy(ctx):
     click.echo('start to deploy')
 
 
 @cmd.command("build")
-def build():
+@click.pass_context
+def build(ctx):
     click.echo('start to build contents')
+    cli_factory: CliFactory = ctx.obj['factory']
+    paten_app = cli_factory.load_paten_app()
+    paten_app.export()
 
 
 @cmd.command("local")
