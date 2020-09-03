98373d40f3e9bbfef48469e775a397b84080d252
yoshiki <yoshiki0911@gmail.com>
2020-09-03 14:34:25 +0900

    #4 example_functionの修正

--- paten/constants.py
+++ paten/constants.py
@@ -6,13 +6,13 @@ app = Paten('%s')
 
 @app.http_trigger('req', methods=['GET'], route='/')
 @app.out_http()
-def example_function(req):
+def example_http_function(req):
     pass
 
 
 @app.http_trigger('req', methods=['GET'], route='/')
 @app.out_queue('arg_name', queue_name='example_queue')
-def example_function(req, arg_name):
+def example_http_queue_function(req, arg_name):
     pass
 """
 
