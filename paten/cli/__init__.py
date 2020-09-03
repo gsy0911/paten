1264d0f93b1857c5b218bd9702ad17a9bf67c0cb
yoshiki <yoshiki0911@gmail.com>
2020-09-03 11:49:03 +0900

    #4 新規プロジェクトを作成するコマンド追加

--- paten/cli/__init__.py
+++ paten/cli/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 import click
+
+from paten.constants import TEMPLATE_APP, GITIGNORE
 
 
 @click.group(invoke_without_command=True)
@@ -23,9 +27,22 @@ def local():
     click.echo('hosting at local')
 
 
+def _create_new_project_skeleton(function_app_name: str):
+    paten_dir = os.path.join(function_app_name, '.paten')
+    os.makedirs(paten_dir)
+    with open(os.path.join(function_app_name, 'requirements.txt'), 'w'):
+        pass
+    with open(os.path.join(function_app_name, 'app.py'), 'w') as f:
+        f.write(TEMPLATE_APP % function_app_name)
+    with open(os.path.join(function_app_name, '.gitignore'), 'w') as f:
+        f.write(GITIGNORE)
+
+
 @cmd.command("new-project")
-def new_project():
+@click.argument('function_app_name')
+def new_project(function_app_name: str):
     click.echo('create new project')
+    _create_new_project_skeleton(function_app_name=function_app_name)
 
 
 def main():
